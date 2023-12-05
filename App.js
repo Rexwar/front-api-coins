9a72014d9ca6f5261654eec96b7383344652e7d0
Rexwar <reyvaldesm@gmail.com>
2023-12-05 17:30:09 -0300

    feat: pagina inicio, con axios

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+import axios from 'axios';
+import { Picker } from '@react-native-picker/picker';
 
-export default function App() {
+const CurrencyConverter = () => {
+  const [amount, setAmount] = useState('');
+  const [convertedAmount, setConvertedAmount] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('EUR');
+  const [currencies, setCurrencies] = useState({});
+
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      try {
+        const response = await axios.get('https://api.frankfurter.app/currencies');
+        setCurrencies(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCurrencies();
+  }, []);
+
+  const convertCurrency = async () => {
+    try {
+      const response = await axios.get(`https://api.frankfurter.app/latest?from=USD&to=${selectedCurrency}&amount=${amount}`);
+      setConvertedAmount(response.data.rates[selectedCurrency]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>USD to Currency Converter</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        value={amount}
+        onChangeText={setAmount}
+        placeholder="Enter USD amount"
+      />
+      <Picker
+        selectedValue={selectedCurrency}
+        style={styles.picker}
+        onValueChange={(itemValue) => setSelectedCurrency(itemValue)}
+      >
+        {Object.entries(currencies).map(([code, name]) => (
+          <Picker.Item style={styles.pickerItem} key={code} label={`${name} (${code})`} value={code} />
+        ))}
+      </Picker>
+      <Button mode="contained" onPress={convertCurrency}>
+        Convert
+      </Button>
+      {convertedAmount && <Text>Convaerted Amount: {convertedAmount} {selectedCurrency}</Text>}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: '100%',
+    padding: 10,
+    marginVertical: 10,
+  },
+  picker: {
+    width: '100%',
+    marginBottom: 10,
+    backgroundColor: '#ccc',
+  },
+  'pickerItem': {
+    fontSize: 12,
   },
 });
+
+export default CurrencyConverter;
